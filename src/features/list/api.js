4cba8e9fb597b84e6cf5be154acc2ace21ef8c57
sebastian tobon <setohe.09@gmail.com>
2018-08-12 18:15:27 -0500

    changes in readme file

--- src/features/list/api.js
+++ src/features/list/api.js
@@ -3,7 +3,7 @@ import axios from "axios/index";
 const api = {
 
   getAll() {
-    return axios.get(`${process.env.API_URL}/gists/public/`);
+    return axios.get(`${process.env.API_URL}/gists/public`);
   }
 
 };
