13ed56a3c083f76c203566de0532e6c9ece4d9ed
sebastian tobon <setohe.09@gmail.com>
2018-08-13 02:19:55 -0500

    changes in markup

--- src/features/list/container/list.js
+++ src/features/list/container/list.js
@@ -30,81 +30,57 @@ class ListContainer extends Component {
   render() {
     const { ListR } = this.props;
     return (
-      <div className="">
+      <div>
         <div className="row">
           <div className="col-4 offset-2">
             <button type="button" className="btn btn-lg" onClick={(e) => this.getAll(e)}>
-              Get All
+              Show all gists
             </button>
           </div>
           <div className="col-4 pull-right">
-            <input type="text" onKeyUp={(e) => this.getByUser(e)} className="form-control margin-t-6" placeholder="Get By User" />
+            <input type="text" onKeyUp={(e) => this.getByUser(e)} className="form-control margin-t-6" placeholder="Search by user" />
           </div>
         </div>
         <br />
-        <div className="row">
-          <div className="col-12">
-            {
-              ListR.allGists.length > 0 &&
-                ListR.allGists.map((item, index) => {
-                  return (
-                    <div key={index} className="card">
-                      <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                          <div className="row">
-                            <div className="col-2">
-                              { item.owner &&
-                                <img src={item.owner.avatar_url} alt={index} />
-                              }
-                            </div>
-                            <div className="col-4">
-                              <label>
-                                {item.description}
-                              </label>
-                            </div>
-                            <div className="col-3">
-                              <label>
-                                {item.updated_at}
-                              </label>
-                            </div>
-                            <div className="col-2">
-                              <label>
-                                {item.comments}
-                              </label>
-                            </div>
-                            <div className="col-1">
-                              <button 
-                                type="button" 
-                                className="btn btn-lg" 
-                                onClick={(e) => this.showDetails(e, item.id)}
-                              >
-                                <i className="fa fa-chevron-circle-right" />
-                              </button>
-                            </div>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                  );
-                })
-            }
-          </div>
-        </div>
-
         <ul className="cards">
-          <li className="cards__item">
-              <div className="card">
-                <div className="card__image card__image--fence"></div>
-                <div className="card__content">
-                    <div className="card__title">Flex</div>
-                    <p className="card__text">This is the shorthand for flex-grow, flex-shrink and flex-basis combined. The second and third parameters (flex-shrink and flex-basis) are optional. Default is 0 1 auto. </p>
-                    <button className="btn btn--block card__btn">Button</button>
-                </div>
-              </div>
-          </li>
+            { ListR.allGists.length > 0 &&
+              ListR.allGists.map((item, index) => {
+                return (
+                  <li key={index} className="cards__item">
+                    <div className="card">
+                      { item.owner ?
+                          <div 
+                            className="card__image" 
+                            style={{ backgroundImage: `url(${item.owner.avatar_url})`}} />
+                          :
+                          <div className="card__image card__image--default" />
+                      }
+                      <div className="card__content">`
+                        <div className="card__title">{item.owner.login}</div>
+                        <p className="card__text">
+                          {item.description}
+                          <br />
+                          <label>
+                            <strong>Comments: </strong>{item.comments}
+                          </label>
+                          <br />
+                          <label>
+                            <strong>Last update: </strong>{item.updated_at}
+                          </label>
+                        </p>
+                        <button 
+                          onClick={(e) => this.showDetails(e, item.id)} 
+                          className="btn btn--block card__btn"
+                        >
+                          Details
+                        </button>
+                      </div>
+                    </div>
+                </li>);
+              })
+            }
         </ul>
       </div>
-      
     );
   }
 }
