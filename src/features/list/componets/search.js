6fe9ccf909524081625c450ec364819d3dd31f7f
sebastian tobon <setohe.09@gmail.com>
2018-08-13 02:45:18 -0500

    Markup and styling changes

--- src/features/list/componets/search.js
+++ src/features/list/componets/search.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const SearchGists = (props) => {
   return (
-    <div className="PlayPause">
+    <div>
        <div className="row">
-          <div className="col-4 offset-2">
+          <div>
             <button 
               type="button" 
-              className="btn btn-lg" 
+              className="btn blue" 
               onClick={(e) => props.getAll(e)}
             >
               Show all gists
             </button>
           </div>
-          <div className="col-4 pull-right">
+          <div className="col-md-6 pull-right">
             <input 
               type="text" onKeyUp={(e) => props.getByUser(e)} 
               className="form-control margin-t-6" 
